bf0c569bf4b33230df1135033e6ab04fbb1777ce
filipjarek <filipjarek24@gmail.com>
2023-04-08 13:29:18 +0200

    Updated RegistrationController and template/registration

--- project/src/Controller/RegistrationController.php
+++ project/src/Controller/RegistrationController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $em): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $user->setRoles(['ROLE_USER']);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
