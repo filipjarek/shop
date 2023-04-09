ec62b778dafb762ea0bc9ccc5ea4d61ecaf2df9d
filipjarek <filipjarek24@gmail.com>
2023-04-09 23:22:03 +0200

    Added DataFixtures/UserFixtures

---
+++ project/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setFirstname('Rick')
+            ->setLastname('Joe')
+            ->setEmail('admin@gmail.com')
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
+            ->setPassword(
+                $this->hasher->hashPassword($user, 'password')
+            );
+        
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
