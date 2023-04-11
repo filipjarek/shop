03a4f8598fbe0bfec5e1a09fa2125508a44a7324
filipjarek <filipjarek24@gmail.com>
2023-04-11 20:43:41 +0200

    TailwindCSS instalation

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -57,7 +57,8 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
