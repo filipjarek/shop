acb52fdae890a21922f06c6b9e2f978b68b22e4b
filipjarek <filipjarek24@gmail.com>
2023-04-13 22:48:54 +0200

    Updated HomeController and template/home

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 }
